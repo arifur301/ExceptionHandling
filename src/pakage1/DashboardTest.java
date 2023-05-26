f8226c6f7ee5739e349adc0fc6ed206f3830c9cd
arifur <arifur.ade@gmail.com>
2023-05-25 22:44:48 -0500

    Now saved it

--- src/pakage1/DashboardTest.java
+++ src/pakage1/DashboardTest.java
@@ -2,4 +2,10 @@ package pakage1;
 
 public class DashboardTest {
 
+	public void dashboardPageShouldDisplay() {
+		String name = "Stanley";
+		String name1 = "Mike";
+		System.out.println(name);
+		System.out.println(name1);
+	}
 }
